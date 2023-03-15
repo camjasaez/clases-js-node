bd7b8b740235822cac35e8dcf2db84dde2ee7fe2
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-03-15 00:52:08 -0300

    Añadiendo los modulos core

--- nodejs/modulosCore/core.js
+++ nodejs/modulosCore/core.js
@@ -3,23 +3,64 @@
     * Modulos core de Node.js son modulos que vienen con la instalacion de Node.js
     * Estos modulos no necesitan ser instalados
     * Estos modulos son:
+    
+    ?   path: Este módulo proporciona una API para trabajar con rutas de archivos y directorios. 
+    ?         Permite crear, unir, resolver y normalizar rutas de archivos y directorios.
+
     ?   fs: Este módulo proporciona una API para interactuar con el sistema de archivos del sistema operativo.
     ?       Permite crear, leer, escribir, actualizar y eliminar archivos y directorios.
 
+
+    ?   os: Este módulo proporciona una API para interactuar con el sistema operativo. Permite obtener información del sistema,
+    ?       como la cantidad de memoria disponible, la carga del sistema, el tipo de plataforma, etc.
+    
+    ?  url: Este módulo proporciona una API para trabajar con URLs. Permite crear, analizar y transformar URLs.
+    ?       Permite obtener información de una URL, como el protocolo, el host, el puerto, etc.
+    
+    ?   events: Este módulo proporciona una API para crear y manejar eventos. 
+    ?           Permite crear, emitir y escuchar eventos personalizados.
+    
     ?   http: Este módulo proporciona una API para crear servidores HTTP y clientes HTTP en Node.js.
     ?         Es útil para crear aplicaciones web y API RESTful.
 
-    ?   path: Este módulo proporciona una API para trabajar con rutas de archivos y directorios. 
-    ?         Permite crear, unir, resolver y normalizar rutas de archivos y directorios.
-    
-    ?   os: Este módulo proporciona una API para interactuar con el sistema operativo. Permite obtener información del sistema,
-    ?       como la cantidad de memoria disponible, la carga del sistema, el tipo de plataforma, etc.
-    ?
-    ?   events: Este módulo proporciona una API para crear y manejar eventos. 
-    ?           Permite crear, emitir y escuchar eventos personalizados.
+    ?   util: Este módulo proporciona una API para trabajar con utilidades.
+    ?         Permite crear clases, heredar de otras clases, crear promesas, etc.
+    ?   assert: Este módulo proporciona una API para realizar pruebas de aserción.
+    ?           Permite verificar si una expresión es verdadera o falsa.
+    ?   net: Este módulo proporciona una API para crear servidores TCP y clientes TCP en Node.js.
+    ?        Es útil para crear aplicaciones de red.
+    ?   dns: Este módulo proporciona una API para interactuar con el sistema de nombres de dominio (DNS).
+    ?        Permite resolver nombres de dominio a direcciones IP y viceversa.
+    ?   querystring: Este módulo proporciona una API para trabajar con cadenas de consulta.
+    ?                Permite crear, analizar y transformar cadenas de consulta.
+    ?   readline: Este módulo proporciona una API para leer datos de un flujo de entrada (por ejemplo, stdin) de manera interactiva.
+    ?   zlib: Este módulo proporciona una API para comprimir y descomprimir datos.
+    ?         Permite comprimir y descomprimir datos usando varios algoritmos de compresión.
+    ?   tty: Este módulo proporciona una API para interactuar con la terminal del sistema operativo.
+    ?        Permite leer datos de la terminal y escribir datos en la terminal.
+    ?   vm: Este módulo proporciona una API para crear y ejecutar scripts de JavaScript.
+    ?       Permite crear un contexto de ejecución para un script de JavaScript y ejecutarlo.
+    ?   child_process: Este módulo proporciona una API para crear procesos hijo.
+    ?                  Permite ejecutar comandos de línea y scripts de Node.js en un subproceso separado.
+    ?   cluster: Este módulo proporciona una API para crear procesos de clúster.
+    ?            Permite crear procesos de clúster para distribuir el trabajo entre varios procesos.
+    ?   dgram: Este módulo proporciona una API para crear servidores UDP y clientes UDP en Node.js.
+    ?          Es útil para crear aplicaciones de red.
     ?   crypto: Este módulo proporciona una API para trabajar con criptografía.
     ?           Permite generar y verificar hashes, firmar y verificar datos, generar claves públicas y privadas, etc.      
     ?   stream: Este módulo proporciona una API para crear y manipular flujos de datos.
     ?            Permite leer y escribir datos de manera eficiente y procesar grandes cantidades de datos sin agotar la memoria.
 
+    __dirname: Es una variable global que contiene el nombre del directorio actual.
+    __filename: Es una variable global que contiene el nombre del archivo actual.
+    process: Es una variable global que contiene información sobre el entorno de ejecución de Node.js.
+    Buffer: Es una clase global que proporciona una API para trabajar con datos binarios.
+    setImmediate(): Es una función global que se ejecuta inmediatamente después de la fase de I / O.
+    setTimeout(): Es una función global que se ejecuta después de un cierto tiempo.
+    setInterval(): Es una función global que se ejecuta después de un cierto tiempo y se repite indefinidamente.
+    console: Es una variable global que proporciona una API para interactuar con la consola del sistema operativo.
+    require(): Es una función global que se utiliza para importar módulos.
+    module: Es una variable global que contiene información sobre el módulo actual.
+    exports: Es una variable global que se utiliza para exportar funciones, objetos o primitivos desde un módulo.
+    
 */
